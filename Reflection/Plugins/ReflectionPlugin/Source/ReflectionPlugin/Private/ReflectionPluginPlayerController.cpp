498978fa9bcc5ddb4e22427af53bfc8148d1b74b
linzehui <linzehui@corp.netease.com>
2022-08-26 18:05:29 +0800

    update...

--- Reflection/Plugins/ReflectionPlugin/Source/ReflectionPlugin/Private/ReflectionPluginPlayerController.cpp
+++ Reflection/Plugins/ReflectionPlugin/Source/ReflectionPlugin/Private/ReflectionPluginPlayerController.cpp
@@ -3,21 +3,47 @@
 
 #include "ReflectionPluginPlayerController.h"
 #include "TestActor.h"
+#include "Kismet/GameplayStatics.h"
 
 void AReflectionPluginPlayerController::Test()
 {
-	for (TFieldIterator<FProperty> It(ATestActor::StaticClass()); It; ++It)
-	{
-		FProperty* Property = *It;
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *(Property->GetNameCPP()));
-	}
+	AActor* TmpActor = UGameplayStatics::GetActorOfClass(this, ATestActor::StaticClass());
 
-	UE_LOG(LogTemp, Warning, TEXT("======"));
-
-	for (TFieldIterator<UFunction> It(ATestActor::StaticClass()); It; ++It)
+	if (TmpActor)
 	{
-		UFunction* Func = *It;
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *(Func->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("%f"), ((Cast<ATestActor>(TmpActor))->TestVal));
+		for (TFieldIterator<FProperty> It(ATestActor::StaticClass()); It; ++It)
+		{
+			FProperty* Property = *It;
+			//UE_LOG(LogTemp, Warning, TEXT("%s"), *(Property->GetNameCPP()));
+			if (Property->GetNameCPP() == "TestVal")
+			{
+				auto v = Property->ContainerPtrToValuePtr<float>(TmpActor);
+				UE_LOG(LogTemp, Warning, TEXT("%f"), *v);
+				*v = 678.123f;
+			}
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%f"), ((Cast<ATestActor>(TmpActor))->TestVal));
+
+		UE_LOG(LogTemp, Warning, TEXT("====="));
+
+		for (TFieldIterator<UFunction> It(ATestActor::StaticClass()); It; ++It)
+		{
+			UFunction* Func = *It;
+			//UE_LOG(LogTemp, Warning, TEXT("%s"), *(Func->GetName()));
+			if (Func->GetName() == "TestUFuncOnly")
+			{
+				struct InParams
+				{
+					int32 A;
+					int32 B;
+					int32 Ret;
+				};
+				InParams inParams{ 11, 22 };
+				TmpActor->ProcessEvent(Func, &inParams);
+				UE_LOG(LogTemp, Warning, TEXT("%d"), inParams.Ret);
+			}
+		}
 	}
 
 
